219637976edf15279c529079982c73d557543111
Jack Gong <jackgong48@gmail.com>
2025-01-22 19:59:48 +1300

    FIXED: mobile nav z

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -29,9 +29,8 @@ const Nav = () => {
       </nav>
       <div
         id="mobileNav"
-        className={`fixed left-0 top-0 z-20 flex h-full w-full flex-col items-center justify-center bg-primary text-4xl transition-all ${
-          showMobileNav ? "z-30 opacity-100" : "pointer-events-none opacity-0"
-        }`}
+        className={`fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-primary text-4xl transition-all ${showMobileNav ? "z-30 opacity-100" : "pointer-events-none opacity-0"
+          }`}
       >
         <div className="flex flex-col gap-8">
           {config.pages.map((page) => (
@@ -47,14 +46,12 @@ const Nav = () => {
         </div>
       </div>
       <Icon
-        icon={`${
-          showMobileNav
-            ? "line-md:menu-to-close-transition"
-            : "line-md:close-to-menu-transition"
-        }`}
-        className={`fixed bottom-2 left-1/2 z-[999] -translate-x-1/2 rounded-full bg-primary p-4 transition-all ${
-          showMobileNav ? "bg-secondary text-base-100" : "bg-base-100"
-        } text-4xl md:hidden`}
+        icon={`${showMobileNav
+          ? "line-md:menu-to-close-transition"
+          : "line-md:close-to-menu-transition"
+          }`}
+        className={`fixed bottom-8 left-1/2 z-[999] -translate-x-1/2 rounded-full bg-primary p-4 transition-all ${showMobileNav ? "bg-secondary text-base-100" : "bg-base-100"
+          } text-4xl md:hidden`}
         onClick={() => setShowMobileNav(!showMobileNav)}
       />
     </div>
